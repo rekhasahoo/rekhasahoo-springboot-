bd26dbc8dec1227bc8ca2d18effd3733c7515c9a
rekhasahoo <132289850+rekhasahoo@users.noreply.github.com>
2025-08-29 10:57:20 +0530

    Create FinalQueryPayLoad.java

---
+++ dto/FinalQueryPayLoad.java
@@ -0,0 +1,10 @@
+package com.example.autosolver.dto;
+
+
+public class FinalQueryPayload {
+private String finalQuery;
+public FinalQueryPayload() {}
+public FinalQueryPayload(String finalQuery) { this.finalQuery = finalQuery; }
+public String getFinalQuery() { return finalQuery; }
+public void setFinalQuery(String finalQuery) { this.finalQuery = finalQuery; }
+}
