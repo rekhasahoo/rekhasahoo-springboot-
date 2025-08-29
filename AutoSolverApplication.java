fff7c87c67515cb496bb721d79618183b28e2fb0
rekhasahoo <132289850+rekhasahoo@users.noreply.github.com>
2025-08-29 10:52:32 +0530

    Create AutoSolverApplication.java

---
+++ AutoSolverApplication.java
@@ -0,0 +1,13 @@
+package com.example.autosolver;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AutoSolverApplication {
+public static void main(String[] args) {
+SpringApplication.run(AutoSolverApplication.class, args);
+}
+}
